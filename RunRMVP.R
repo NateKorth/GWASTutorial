0f5b0b99c5d2b460698ac36fd7e17117f03404da
NateKorth <93673488+NateKorth@users.noreply.github.com>
2023-03-03 13:36:47 -0600

    Update RunRMVP.R

--- RunRMVP.R
+++ RunRMVP.R
@@ -7,13 +7,13 @@ covariates_PC<-bigmemory::as.matrix(attach.big.matrix("../input/geno/SAP.pc.desc
 
 #Run a single GWAS
 phenotype2<-phenotype1[1:2]
-imMVP <- MVP(phe=phenotype2,geno=genotype,map=map,K=kinship,CV.FarmCPU=covariates_PC,priority="speed",maxLoop=10,ncpus=64,method.bin="FaST-LMM",method=c("FarmCPU","MLM"))
+imMVP <- MVP(phe=phenotype2,geno=genotype,map=map,K=kinship,CV.FarmCPU=covariates_PC,CV.MLM=covariates_PC,priority="speed",maxLoop=10,ncpus=64,method.bin="FaST-LMM",method=c("FarmCPU","MLM"))
 
 
 #A loop to run all phenotypes
 
 #for(i in 2:ncol(phenotype1)){
-#   imMVP <- MVP(phe=phenotype1[,c(1,i)],geno=genotype,map=map,K=kinship,
+#   imMVP <- MVP(phe=phenotype1[,c(1,i)],geno=genotype,map=map,K=kinship,CV.MLM=covariates_PC,
 #    CV.FarmCPU=covariates_PC,priority="speed",maxLoop=10,ncpus=64,
 #    method.bin="FaST-LMM",method=c("FarmCPU","MLM")
 #  )
